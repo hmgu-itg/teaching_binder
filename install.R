adb9f66a485d406b5f495cd388bbd2216ac25ef8
agilly <ag15@sanger.ac.uk>
2019-06-16 20:38:53 +0200

    reruntime

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
-.libPaths( c( "~/R/packages", .libPaths()) )
-install.packages('IRkernel', lib="~/R/packages")
-IRkernel::installspec()
-install.packages("qqman", lib="~/R/packages")
-install.packages("gap", lib="~/R/packages")
+#.libPaths( c( "~/R/packages", .libPaths()) )
+#install.packages('IRkernel', lib="~/R/packages")
+#IRkernel::installspec()
+install.packages("qqman")
+install.packages("gap")
 
