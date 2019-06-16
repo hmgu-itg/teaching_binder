370e2b33e1d1162e7c9d9646cbe0b576e841aaf4
agilly <ag15@sanger.ac.uk>
2019-06-16 12:47:58 +0200

    rm R environ

---
+++ install.R
@@ -0,0 +1,5 @@
+#!/usr/bin/env Rscript
+.libPaths( c( "~/R/packages", .libPaths()) )
+install.packages('IRkernel', lib="~/R/packages")
+IRkernel::installspec()
+install.packages(c("gap", "qqman", lib="~/R/packages")
